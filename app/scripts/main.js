6f3f31d7629ba5174aa8edcfb4a3eda0ce8009fd
Zakeria Ali <zak.fined@gmail.com>
2017-07-27 01:00:16 +0100

    Style the footer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
     pressed.push(event.key);
     if (pressed.join(", ").includes(secret)) {
       $('.header').css('display', 'none');
+      $('.footer p:first-child').css('display', 'block');
+      $('.footer p:last-child').css('display', 'none');
       $('#video-background').css('display', 'block');
       $('#video-background').html('<source src="video/Scroll.mp4" type="video/mp4">');
       $('body').unbind('keydown');
